9c8ec08ce7c10902b65927a098e1480fca977b3e
Adam Farmer <me@adamfarmer.dev>
2021-02-06 17:56:50 +1100

    checked and unchecked

--- sync.py
+++ sync.py
@@ -7,11 +7,6 @@ class handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         print("Recieved GET")
 
-        checked = False
-        if self.path=="/checked":
-            checked = True
-        
-
         # Sending an '200 OK' response
         self.send_response(200)
 
@@ -32,9 +27,20 @@ class handler(http.server.SimpleHTTPRequestHandler):
         #keep.sync()
 
         note = keep.get("1b3CkvjF0IZ2VnIXTIsaKVzOfg2e1JUo7Qsrbc3ivVXae5ikmxOpIO12rsS2XpA")
+
+        unchecked = ""
+        checked = ""
         for item in note.items:
-            if item.checked == checked:
-                self.wfile.write(bytes(item.text + ",", "utf8"))
+            if item.checked:
+                if len(checked) > 0:
+                    checked += ","
+                checked += item.text
+            else:
+                if len(unchecked) > 0:
+                    unchecked += ","
+                unchecked += item.text
+                
+        self.wfile.write(bytes(unchecked + "|" + checked, "utf8"))
 
         return
 
